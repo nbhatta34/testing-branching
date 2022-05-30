0e05b73e3519c2dd30d3fef223dab21a9b92e1cc
nbhatta34 <niteshbhatta44881@gmail.com>
2022-05-30 11:52:36 +0545

    Completed Create Profile

---
+++ lib/Screens/CreateProfile.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class CreateProfile extends StatefulWidget {
+  const CreateProfile({Key? key}) : super(key: key);
+
+  @override
+  State<CreateProfile> createState() => _CreateProfileState();
+}
+
+class _CreateProfileState extends State<CreateProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      body: Text("Create Profile"),
+    ));
+  }
+}
